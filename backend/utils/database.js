ae42b87fc9ef97f73feedcd1aff25fa117edc15b
MayankTiwari9 <mt1027319@gmail.com>
2024-05-22 23:44:46 +0530

    Updated Code

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize('company-review-system', 'root', 'AnuMayank@123', {
+const sequelize = new Sequelize('company-review-system', 'root', 'Mayank@123', {
     dialect: "mysql",
     host: "localhost"
 })
