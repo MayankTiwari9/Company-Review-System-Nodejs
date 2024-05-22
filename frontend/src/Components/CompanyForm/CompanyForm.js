07c999e16f2510bde73e5f7475eaf840dcf5e4d7
MayankTiwari9 <mt1027319@gmail.com>
2024-05-22 11:03:13 +0530

    Updated Code

--- frontend/src/Components/CompanyForm/CompanyForm.js
+++ frontend/src/Components/CompanyForm/CompanyForm.js
@@ -6,7 +6,7 @@ const CompanyForm = () => {
     name: "",
     pros: "",
     cons: "",
-    rating: 0
+    rating: 0,
   });
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [error, setError] = useState(null);
@@ -17,13 +17,13 @@ const CompanyForm = () => {
     setError(null);
 
     try {
-      const response = await axios.post('http://localhost:4000/add-company', formData);
+      await axios.post("http://localhost:4000/add-company", formData);
       alert("Company Review Added Successfully");
       setFormData({
         name: "",
         pros: "",
         cons: "",
-        rating: 0
+        rating: 0,
       });
     } catch (err) {
       setError("An error occurred while adding the review. Please try again.");
@@ -42,46 +42,82 @@ const CompanyForm = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={formSubmitHandler}>
-        <div>
-          <label htmlFor="name">Company Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Company Name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
+    <form onSubmit={formSubmitHandler}>
+      <div className="space-y-12 p-6">
+        <h1 className="text-base flex mx-auto font-semibold leading-7 text-gray-900">
+          Company Review
+        </h1>
+
+        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="sm:col-span-3">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Company name
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                autoComplete="given-name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+                className="block w-2/4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
         </div>
-        <div>
-          <label htmlFor="pros">Pros</label>
-          <textarea
-            id="pros"
-            name="pros"
-            placeholder="Pros"
-            value={formData.pros}
-            onChange={handleChange}
-            required
-          />
+        <div className="flex mt-10  gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="col-span-full">
+            <label
+              htmlFor="pros"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Pros
+            </label>
+            <div className="mt-2">
+              <textarea
+                id="pros"
+                name="pros"
+                rows={3}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={formData.pros}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <div className="col-span-full">
+            <label
+              htmlFor="cons"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Cons
+            </label>
+            <div className="mt-2">
+              <textarea
+                id="cons"
+                name="cons"
+                rows={3}
+                value={formData.cons}
+                onChange={handleChange}
+                required
+                className="block full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                defaultValue={""}
+              />
+            </div>
+          </div>
         </div>
-        <div>
-          <label htmlFor="cons">Cons</label>
-          <textarea
-            id="cons"
-            name="cons"
-            placeholder="Cons"
-            value={formData.cons}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
+        <div className="flex gap-x-6 gap-y-8 mt-10 space-y-10">
           <label>Rating</label>
           {[1, 2, 3, 4, 5].map((num) => (
-            <label key={num}>
+            <label
+              key={num}
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
               <input
                 type="radio"
                 name="rating"
@@ -89,17 +125,25 @@ const CompanyForm = () => {
                 checked={formData.rating === num}
                 onChange={handleChange}
                 required
+                className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
               />
               {num}
             </label>
           ))}
         </div>
-        <button type="submit" disabled={isSubmitting}>
-          {isSubmitting ? "Submitting..." : "Add Review"}
-        </button>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-      </form>
-    </div>
+
+        <div className="m-6 flex items-center justify-start gap-x-6">
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            {isSubmitting ? "Submitting..." : "Add Review"}
+          </button>
+          {error && <p style={{ color: "red" }}>{error}</p>}
+        </div>
+      </div>
+    </form>
   );
 };
 
--- frontend/src/Components/SearchedCompany/SearchedCompany.js
+++ frontend/src/Components/SearchedCompany/SearchedCompany.js
@@ -35,23 +35,73 @@ const SearchedCompany = () => {
   return (
     <div>
       <form onSubmit={searchedCompanyHandler}>
-        <input
-          type="text"
-          onChange={(e) => setCompany(e.target.value)}
-          value={company}
-          placeholder="Search for a company"
-        />
-        <button type="submit">Search</button>
-      </form>
-      <h1>Search Results for: {company}</h1>
-      <h2>Average Rating: {averageRating.toFixed(1)}</h2>
-      {searchedCompanies.map((comp, index) => (
-        <div key={index}>
-          <p>Pros: {comp.pros}</p>
-          <p>Cons: {comp.cons}</p>
-          <p>Rating: {comp.rating}</p>
+        <div className="sm:col-span-3">
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Search For The Company
+          </label>
+          <div className="mt-2">
+            <input
+              type="text"
+              name="name"
+              id="name"
+              onChange={(e) => setCompany(e.target.value)}
+              value={company}
+              required
+              className="block w-1/4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
         </div>
-      ))}
+        <button
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          type="submit"
+        >
+          Search
+        </button>
+      </form>
+      <div className="bg-white py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Search Results for: {company}
+            </h1>
+            <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+              Average Rating: {averageRating.toFixed(1)}
+            </h2>
+          </div>
+          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            {searchedCompanies.map((comp) => (
+              <article
+                key={comp.id}
+                className="flex max-w-xl flex-col items-start justify-between"
+              >
+                <div className="group relative">
+                  <h6 className="text-2xl font-thin tracking-tight text-gray-900 sm:text-3xl">
+                    Pros:-{" "}
+                  </h6>
+                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                    {comp.pros}
+                  </p>
+                  <h6 className="text-2xl font-thin tracking-tight text-gray-900 sm:text-3xl">
+                    Cons:-{" "}
+                  </h6>
+                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                    {comp.cons}
+                  </p>
+                  <h6 className="text-2xl font-thin tracking-tight text-gray-900 sm:text-3xl">
+                    Ratings:-{" "}
+                  </h6>
+                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                    {comp.rating}
+                  </p>
+                </div>
+              </article>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
